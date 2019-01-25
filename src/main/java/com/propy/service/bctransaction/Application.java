34c9e325274068fe74e6fb254922f52b3b18081f
MaximFischuk <mfischuk@vareger.com>
2019-01-25 16:55:28 +0200

    Added K8S deploy configuration file

--- src/main/java/com/propy/service/bctransaction/Application.java
+++ src/main/java/com/propy/service/bctransaction/Application.java
@@ -1,33 +1,21 @@
 package com.propy.service.bctransaction;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
-@RestController
 @RefreshScope
 @Slf4j
 public class Application {
 
-    @Value("${test}")
-    public String test;
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @GetMapping("/")
-    public String get() {
-        return this.test;
     }
 
 }
